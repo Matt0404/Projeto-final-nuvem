f5f2b036202a81aebc846a96a24afbbe78b23660
Matilde <matilde.g.f.rodrigues@gmail.com>
2026-06-10 16:58:02 +0100

    commit

--- infrastructure/terraform/environments/dev/output.tf
+++ infrastructure/terraform/environments/dev/output.tf
@@ -22,3 +22,12 @@ output "api_gateway_public_ip" {
   description = "IP público da EC2 do api-gateway"
   value       = module.mini-ecommerce-dev-api_gateway.public_ip
 }
+
+output "ec2_ips" {
+  value = {
+    api_gateway          = module.api_gateway_ec2.public_ip
+    catalog_service      = module.catalog_service_ec2.public_ip
+    order_service        = module.order_service_ec2.public_ip
+    notification_service = module.notification_service_ec2.public_ip
+  }
+}
