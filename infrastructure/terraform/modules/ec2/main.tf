6e2ac9d42a4bfd7e02cba92f5a577cbbd2bbde46
Matilde <matilde.g.f.rodrigues@gmail.com>
2026-06-20 15:59:06 +0100

    ec2

--- infrastructure/terraform/modules/ec2/main.tf
+++ infrastructure/terraform/modules/ec2/main.tf
@@ -15,16 +15,42 @@ locals {
     ManagedBy = "terraform"
   })
 }
- 
+
+# IAM Role
+resource "aws_iam_role" "ec2_ecr_role" {
+  name = "${var.name}-ecr-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read" {
+  role       = aws_iam_role.ec2_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ec2_ecr_profile" {
+  name = "${var.name}-ecr-profile"
+  role = aws_iam_role.ec2_ecr_role.name
+}
+
 resource "aws_instance" "this" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.web_sg_id]
-  key_name               = var.key_name
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.web_sg_id]
+  key_name                    = var.key_name
   associate_public_ip_address = true
-  user_data = var.user_data
- 
- 
+  user_data                   = var.user_data
+  iam_instance_profile        = aws_iam_instance_profile.ec2_ecr_profile.name
+
   tags = local.common_tags
 }
